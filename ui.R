1de04f802998f55c7f677783a5a6e885abaf6fd0
aaronlieu <44924872+aaronlieu@users.noreply.github.com>
2020-04-10 12:48:24 +0800

    Adjustment - making the map width 100% and both panels become absolutepanels with opac - added icon to titlepanel (but not displaying) - adjusted the opac for the panels

--- ui.R
+++ ui.R
@@ -8,19 +8,19 @@ library(plotly)
 library(plyr)
 
 
-ui <- fluidPage(titlePanel(h1("3-Starred Michelin Restaurants", align = "center")),
+ui <- fluidPage(titlePanel( 
+                  title = 
+                    div(align = "center", h1(img(src="C:/Users/user/Desktop/IVP/star1.png"), "3-Starred Michelin Restaurants"))
+                    
+                ),
                 fluidRow(
                   tags$head(includeCSS("styles.css")),
-                  
-                  column(10,
-                         leafletOutput(outputId = "mymap",height="800"),
-                         
-                  ),
+                  leafletOutput(outputId = "mymap",height="950px"),
                   
                   absolutePanel(
                     id = "controls", class = "panel panel-default",
-                    top = 80, left = 20, width = 250, fixed=TRUE,
-                    draggable = TRUE, height = "auto",
+                    top = "13%", left = "1%", width = "17%", fixed=TRUE,
+                    draggable = FALSE, height = "800px",
                     tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: #9A1F33;border-top: 1px solid #9A1F33;border-bottom: 1px solid #9A1F33;} .irs-from, .irs-to, .irs-single { background: #9A1F33 }")),
                     tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar {background: #9A1F33;border-top: 1px solid #9A1F33;border-bottom: 1px solid #9A1F33;} .irs-from, .irs-to, .irs-single { background: #9A1F33 }")),
                     
@@ -37,23 +37,25 @@ ui <- fluidPage(titlePanel(h1("3-Starred Michelin Restaurants", align = "center"
                     #Preference selector
                     checkboxInput("vegetarianInput", p("Vegetarian",style="font-weight:bold"), FALSE),
                     checkboxInput("outdoorInput", p("Outdoor Dining",style="font-weight:bold"), FALSE),
-                    
-                    
-                    
-                    div( style = "position: relative; top: 50px;height:420px;position:absolute; top:370px;width:100%;left:5px;", 
-                         plotlyOutput("Country", 
-                                      width = "100%"
-                         ),
-                    )
-                    
-                  ),
                   
-                  column(2,p("Restaurant List",style = "font-weight: bold;"),
+                    div( style = "/*position: relative; top: 50px;height:420px;position:absolute; top:370px;width:100%;left:5px; */",
+                          plotlyOutput("Country", width = "100%"),
+                    )
+                  ),  
+                    
+                  absolutePanel(
+                      id = "controls", class = "panel panel-default",
+                      top = "13%", right = "1%", width = "12%", fixed=TRUE,
+                      draggable = FALSE, height = "350px",
+                      tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: #9A1F33;border-top: 1px solid #9A1F33;border-bottom: 1px solid #9A1F33;} .irs-from, .irs-to, .irs-single { background: #9A1F33 }")),
+                      tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar {background: #9A1F33;border-top: 1px solid #9A1F33;border-bottom: 1px solid #9A1F33;} .irs-from, .irs-to, .irs-single { background: #9A1F33 }")),
+                  
+                         p("Restaurant List",style = "font-weight: bold;"),
                          div(style = "overflow-y: auto; height:300px; background-color:#f0dcdf;position:relative; top:0px;left:0;", uiOutput("restaurantOutput")),
-                         div(style = "overflow-x: auto;height:500px; width:100%;position:absolute; top:350px;right:15px;", plotOutput("resOutput",width = "100%", dblclick = "plot2_dblclick",
-                                                                                                                                      brush = brushOpts(id = "plot2_brush", resetOnNew = TRUE), 
-                                                                                                                                      click = "plot2_click"), uiOutput("dynamic")
+                         div(style = "overflow-x: auto;height:500px; width:100%;position:absolute; top:350px;right:15px;", plotlyOutput("resOutput",width = "100%"), 
+                                                                                                                                      click = "plot2_click", uiOutput("dynamic")
                          ),
                          
-                  )),
-)
+                  ),
+
+))
